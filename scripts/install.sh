846b435c08654ecb45d8ab41291dd1598ba8dd45
Paulo Cabral <sumolx@gmail.com>
2023-05-03 07:09:47 -0400

    Added cleanup of previous install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-mkdir -p /mnt/app/services/scripts
+#remove old files
+rm -rf /mnt/app/services/dropbear
+rm -rf /mnt/app/services/scripts
+
+#install new files
 cp -a dropbear /mnt/app/services/
 cp -a scripts /mnt/app/services/
 cp -a startup.sh /mnt/app/services/
