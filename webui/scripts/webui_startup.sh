b498756e4bbf6ceecac42fdead2b4d8623f952ab
Paulo Cabral <sumolx@gmail.com>
2023-08-05 20:34:24 -0400

    Script has been superceded by wlan_start_www.sh

--- webui/scripts/webui_startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-ln -sfn /mnt/app/services/www/busybox /mnt/app/services/www/httpd
-ln -sfn /mnt/extsd/movies /mnt/app/services/www/movies
-httpd -v -h /mnt/app/services/www > /tmp/www.log 2>&1
