e748024fd9c5605b6b59533de00796ffec04d512
Paulo Cabral <sumolx@gmail.com>
2023-08-15 11:40:52 -0400

    Fixed library installation of startup scripts

--- ffmpeg/scripts/50_ffmpeg_startup.sh
+++ ffmpeg/scripts/50_ffmpeg_startup.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/app/services/ffmpeg/lib
+ln -sfn /mnt/app/services/ffmpeg/lib/libavcodec.so.59.18.100 /mnt/app/services/ffmpeg/lib/libavcodec.so.59
+ln -sfn /mnt/app/services/ffmpeg/lib/libavcodec.so.59 /mnt/app/services/ffmpeg/lib/libavcodec.so
+ln -sfn /mnt/app/services/ffmpeg/lib/libavcodec.so /lib/libavcodec.so
 
-ln -sfn /mnt/app/services/ffmpeg/ffmpeg /bin/ffmpeg
-ln -sfn /mnt/app/services/ffmpeg/ffprobe /bin/ffprobe
+ln -sfn /mnt/app/services/ffmpeg/lib/libavdevice.so.59.4.100 /mnt/app/services/ffmpeg/lib/libavdevice.so.59
+ln -sfn /mnt/app/services/ffmpeg/lib/libavdevice.so.59 /mnt/app/services/ffmpeg/lib/libavdevice.so
+ln -sfn /mnt/app/services/ffmpeg/lib/libavdevice.so /lib/libavdevice.so
+
+ln -sfn /mnt/app/services/ffmpeg/lib/libavfilter.so.8.24.100 /mnt/app/services/ffmpeg/lib/libavfilter.so.8
+ln -sfn /mnt/app/services/ffmpeg/lib/libavfilter.so.8 /mnt/app/services/ffmpeg/lib/libavfilter.so
+ln -sfn /mnt/app/services/ffmpeg/lib/libavfilter.so /lib/libavfilter.so
+
+ln -sfn /mnt/app/services/ffmpeg/lib/libavformat.so.59.16.100 /mnt/app/services/ffmpeg/lib/libavformat.so.59
+ln -sfn /mnt/app/services/ffmpeg/lib/libavformat.so.59 /mnt/app/services/ffmpeg/lib/libavformat.so
+ln -sfn /mnt/app/services/ffmpeg/lib/libavformat.so /lib/libavformat.so
+
+ln -sfn /mnt/app/services/ffmpeg/lib/libavutil.so.57.17.100 /mnt/app/services/ffmpeg/lib/libavutil.so.57
+ln -sfn /mnt/app/services/ffmpeg/lib/libavutil.so.57 /mnt/app/services/ffmpeg/lib/libavutil.so
+ln -sfn /mnt/app/services/ffmpeg/lib/libavutil.so /lib/libavutil.so
+
+ln -sfn /mnt/app/services/ffmpeg/bin/ffmpeg /bin/ffmpeg
+ln -sfn /mnt/app/services/ffmpeg/bin/ffprobe /bin/ffprobe
 
