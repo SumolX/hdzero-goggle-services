9e397e69b3b6c3731b8e83e8728ac1b61238d4bf
Paulo Cabral <sumolx@gmail.com>
2023-08-31 17:11:01 -0400

    Fixed installation issues

--- ffmpeg/scripts/50_ffmpeg_startup.sh
+++ ffmpeg/scripts/50_ffmpeg_startup.sh
@@ -1,24 +1,11 @@
 #!/bin/sh
 
-ln -sfn /mnt/app/services/ffmpeg/lib/libavcodec.so.59.18.100 /mnt/app/services/ffmpeg/lib/libavcodec.so.59
-ln -sfn /mnt/app/services/ffmpeg/lib/libavcodec.so.59 /mnt/app/services/ffmpeg/lib/libavcodec.so
-ln -sfn /mnt/app/services/ffmpeg/lib/libavcodec.so /lib/libavcodec.so
-
-ln -sfn /mnt/app/services/ffmpeg/lib/libavdevice.so.59.4.100 /mnt/app/services/ffmpeg/lib/libavdevice.so.59
-ln -sfn /mnt/app/services/ffmpeg/lib/libavdevice.so.59 /mnt/app/services/ffmpeg/lib/libavdevice.so
-ln -sfn /mnt/app/services/ffmpeg/lib/libavdevice.so /lib/libavdevice.so
-
-ln -sfn /mnt/app/services/ffmpeg/lib/libavfilter.so.8.24.100 /mnt/app/services/ffmpeg/lib/libavfilter.so.8
-ln -sfn /mnt/app/services/ffmpeg/lib/libavfilter.so.8 /mnt/app/services/ffmpeg/lib/libavfilter.so
-ln -sfn /mnt/app/services/ffmpeg/lib/libavfilter.so /lib/libavfilter.so
-
-ln -sfn /mnt/app/services/ffmpeg/lib/libavformat.so.59.16.100 /mnt/app/services/ffmpeg/lib/libavformat.so.59
-ln -sfn /mnt/app/services/ffmpeg/lib/libavformat.so.59 /mnt/app/services/ffmpeg/lib/libavformat.so
-ln -sfn /mnt/app/services/ffmpeg/lib/libavformat.so /lib/libavformat.so
-
-ln -sfn /mnt/app/services/ffmpeg/lib/libavutil.so.57.17.100 /mnt/app/services/ffmpeg/lib/libavutil.so.57
-ln -sfn /mnt/app/services/ffmpeg/lib/libavutil.so.57 /mnt/app/services/ffmpeg/lib/libavutil.so
-ln -sfn /mnt/app/services/ffmpeg/lib/libavutil.so /lib/libavutil.so
+ln -sfn /mnt/app/services/ffmpeg/lib/libavcodec.so.59 /mnt/app/services/ffmpeg/lib/libavcodec.so.59
+ln -sfn /mnt/app/services/ffmpeg/lib/libavdevice.so.59 /mnt/app/services/ffmpeg/lib/libavdevice.so.59
+ln -sfn /mnt/app/services/ffmpeg/lib/libavfilter.so.8 /mnt/app/services/ffmpeg/lib/libavfilter.so.8
+ln -sfn /mnt/app/services/ffmpeg/lib/libavformat.so.59 /mnt/app/services/ffmpeg/lib/libavformat.so.59
+ln -sfn /mnt/app/services/ffmpeg/lib/libavutil.so.57 /mnt/app/services/ffmpeg/lib/libavutil.so.57
+ln -sfn /mnt/app/services/ffmpeg/lib/libpostproc.so.56 /mnt/app/services/ffmpeg/lib/libpostproc.so.56
 
 ln -sfn /mnt/app/services/ffmpeg/bin/ffmpeg /bin/ffmpeg
 ln -sfn /mnt/app/services/ffmpeg/bin/ffprobe /bin/ffprobe
